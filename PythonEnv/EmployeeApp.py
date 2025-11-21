d0ef5bf793dc0f4918e3c910b6a4c1a2afeb0421
AlexPham5 <113805467+AlexPham5@users.noreply.github.com>
2025-11-21 16:41:32 -0600

    added more checks for inputs and better formatting

--- PythonEnv/EmployeeApp.py
+++ PythonEnv/EmployeeApp.py
@@ -1,30 +1,8 @@
-## User Stories
-
-### Employee App (Python)
-#- As an employee, I want to log in with my credentials so that I can securely access my 
-#  expense reports.
-#- As an employee, I want to submit a new expense with details about amount and 
-#  description so that I can request reimbursement or track spending.
-#- As an employee, I want to view the status of my submitted expenses so that I know 
-#  whether they are pending, approved, or denied.
-#- As an employee, I want to edit or delete expenses that are still pending so that I 
-#  can correct mistakes before they are reviewed.
-#- As an employee, I want to view a history of all my approved and denied expenses 
-#  so that I can track my financial activity over time.
-
-# Employee credentials dictionary
-#   - employeeCredentials = {'ID':['Username1','Password1','Role']}
-# Expenses dictionary (will be with SQLite in actual project)
-#   - expenses = {ID:[UserID, Amount, 'Description', Date]}
-# History database? Whenever expenses are approved/denied, add them to the history database
-
 # Features to be added:
 # Use logging instead of print statements for information (successful login, errors, successful add to db)
 # checks for amount input/edit that the value is not negative
 # checks for description to require a minimum length (10 chars)
 # check for date to not add a future date?
-# displaying information better, with pandas dataframe?
-
 
 import sqlite3
 import sys
@@ -35,15 +13,13 @@ from helpers import helper
 class EmployeeApp:
     def __init__(self):
         # Logging
-        logging.basicConfig(filename="C:\\Users\\alex1\\Revature work\\Project 0\\pythonlog.log",
+        logging.basicConfig(filename="C:\\Users\\alex1\\Revature_work\\Project_0\\pythonlog.log",
                             level=logging.INFO,
                             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         logging.info("LOG START")
-
         # stores userID from login prompt
         self.userID = -1
-        #self.dbPath = "..\\RevatureDatabase.db"
-        self.dbPath = "C:\\Users\\alex1\\Revature work\\Project 0\\RevatureDatabase.db"
+        self.dbPath = "C:\\Users\\alex1\\Revature_work\\Project_0\\RevatureDatabase.db"
     
     def __del__(self):
         logging.info("LOG END")
@@ -54,7 +30,6 @@ class EmployeeApp:
         while(1):
             self.login()
             while(1):
-                # Then offer command options
                 print("===== COMMAND OPTIONS =====\n" \
                 "1 - Submit a new expense\n" \
                 "2 - View all submitted expenses\n" \
@@ -93,7 +68,7 @@ class EmployeeApp:
                 "2 - EXIT")
                 userInput = int(input("Please enter a number: "))
             except ValueError:
-                print("Invalid input, value error occurred")
+                print("Invalid input for login choice, please enter 1 or 2\n")
                 logging.error("Value error for login choice input")
             else:
                 if(userInput == 1):
@@ -137,7 +112,7 @@ class EmployeeApp:
                     sys.exit()
                 else:
                     logging.warning("Invalid number input for login choice")
-                    print("Invalid number input for login options")
+                    print("Invalid number input for login options, please enter 1 or 2\n")
         return
 
     #- As an employee, I want to submit a new expense with details about amount and 
@@ -145,38 +120,45 @@ class EmployeeApp:
     def submitExpense(self):
         #add a value to the expenses table
         try:
-                amountInput = float(input("Enter an amount for this expense: "))
-                descInput = input("Enter a reason for the expense request: ")
+                amountInput = float(input("Enter an amount for this expense (at least $1): "))
+                if(amountInput <= 0):
+                    print("Amount must be greater than $0")
+                    logging.warning("Invalid input for amount field in new expense")
+                    raise ValueError
+                
+                descInput = input("Enter a reason for the expense request (at least 10 characters): ")
+                if(len(descInput) < 10):
+                    print("Description must be at least 10 characters long")
+                    logging.warning("Invalid input for description in submit expense")
+                    raise ValueError
+                
                 print("Entering new date for expense: ")
                 logging.info("Entering new data for expenses")
                 dateInputY = input("Enter a year as 4 digits: ")
-                if(len(dateInputY) > 4):
-                    print("Invalid input for year, must be 4 digits")
+                if(len(dateInputY) != 4 or int(dateInputY) > 2025):
+                    print("Invalid input for year")
                     logging.warning("Invalid input for year field in new expense")
                     raise ValueError
-                dateInputM = input("Enter a month as 2 digits: ")
-                if(len(dateInputM) != 2 or int(dateInputM) < 1 or int(dateInputM) > 12):
+                dateInputM = input("Enter a month as digits: ")
+                if(int(dateInputM) < 1 or int(dateInputM) > 12):
                     print("Invalid input for month")
                     logging.warning("Invalid input for month in new expense")
                     raise ValueError
-                dateInputD = input("Enter a day as 2 digits: ")
-                if(len(dateInputD) != 2 or int(dateInputD) < 1 or int(dateInputD) > 31):
+                dateInputM = f"{int(dateInputM):0{2}d}"
+                dateInputD = input("Enter a day as digits: ")
+                if(int(dateInputD) < 1 or int(dateInputD) > 31):
                     print("Invalid input for day")
                     logging.warning("Invalid input for day field in new expense")
                     raise ValueError
+                dateInputD = f"{int(dateInputD):0{2}d}"
                 dateInput = dateInputY + "-" + dateInputM + "-" + dateInputD
         except ValueError:
-            print("Invalid input for submitExpense, ValueError")       
+            print("Invalid input for submit expense\n")       
         else:
-            #print(amountInput)
-            #print(descInput)
-            #print(dateInput)
-            # actually add the expense to the database
             try:
                 with sqlite3.connect(self.dbPath) as conn:
                     cursor = conn.cursor()
-                    
-                    # Add to the expenses table
+
                     addExpense =f"""
                     INSERT INTO expenses (user_id, amount, description, date)
                     VALUES ('{self.userID}', '{amountInput}', '{descInput}', '{dateInput}')
@@ -198,8 +180,8 @@ class EmployeeApp:
                     cursor.execute(addApproval)
                     conn.commit()
             except sqlite3.Error as error:
-                print("Error occured - ", error)
-                logging.error("SQL error occured in submit expense- ", error)
+                print("SQL Error occured: \n", error)
+                logging.error("SQL error occured in submit expense")
             else:
                 print("Successfully added new expense and approval pending manager review")
                 logging.info("Employee successfully submitted new expense")
@@ -230,11 +212,11 @@ class EmployeeApp:
                     amountString = ("$%.2f"%(row[2]))
                     tablerow = [row[0], amountString, row[3], row[4], status[0]]
                     data.append(tablerow)
-                print(tabulate(data))
+                print(tabulate(data, tablefmt="grid"))
                 logging.info("Successfully showed existing user expenses")
         except sqlite3.Error as error:
-            print("Error occured - ", error)
-            logging.error("SQL error occured in view expense- ", error)
+            print("Error occured: \n", error)
+            logging.error("SQL error occured in view expense")
         return
 
     #- As an employee, I want to edit or delete expenses that are still pending so that I 
@@ -266,18 +248,26 @@ class EmployeeApp:
                             if(userInput == 1):
                                 #prompt amount and change it
                                 field = "amount"
-                                newInput = float(input("Enter a new amount: "))
+                                newInput = float(input("Enter a new amount (at least $1): "))
+                                if(newInput <= 0):
+                                    print("Amount value must be at least $1")
+                                    logging.warning("Invalid input for amount in edit expense")
+                                    raise ValueError
                             elif(userInput == 2):
                                 #prompt description and change it
                                 field = "description"
-                                newInput = input("Enter a new description: ")
+                                newInput = input("Enter a new description (at least 10 characters long): ")
+                                if(len(newInput) < 10):
+                                    print("Description must be at least 10 characters long")
+                                    logging.warning("Invalid input for description in edit expense")
+                                    raise ValueError
                             elif(userInput == 3):
                                 #prompt date and change it
                                 field = "date"
                                 print("Entering new date for expense: ")
                                 dateInputY = input("Enter a year as 4 digits: ")
-                                if(len(dateInputY) > 4):
-                                    print("Invalid input for year, must be 4 digits")
+                                if(len(dateInputY) != 4 or int(dateInputY) > 2025):
+                                    print("Invalid input for year")
                                     logging.warning("Invalid input for edit expense year")
                                     raise ValueError
                                 dateInputM = input("Enter a month as 2 digits: ")
@@ -311,14 +301,14 @@ class EmployeeApp:
                             print("Invalid input for field selection")
                             logging.error("Invalid error for field selection in edit expense")
                         except sqlite3.Error as error:
-                            print("SQL Error occured - ", error)
-                            logging.error("SQL Error occured - ", error)
+                            print("SQL Error occured: \n", error)
+                            logging.error("SQL Error occured in edit expense")
         except ValueError:
-            print("Invalid input for editExpense")
+            print("Invalid input for edit ID\n")
             logging.error("Invalid input for expenseID in edit expense")
         except sqlite3.Error as error:
-            print("SQL Error occured- ", error)
-            logging.error("SQL Error occured in edit- ", error)
+            print("SQL Error occured: \n", error)
+            logging.error("SQL Error occured in edit")
         return
     
     def deleteExpense(self):
@@ -339,9 +329,14 @@ class EmployeeApp:
                     cursor.execute(f"SELECT status FROM approvals WHERE expense_id = {expenseID}")
                     status = cursor.fetchone()[0]
                     if(status != "pending"):
-                        raise sqlite3.Error("The expense selected for deletion is not pending, you may only delete pending expenses")
+                        raise Exception("The expense selected for deletion is not pending, you may only delete pending expenses\n")
                     else:
-                        print("The expense seleced for deletion: ID-%i, Amount-%.2f, Description-%s, Date-%s" %(expenseTBD[0], expenseTBD[2], expenseTBD[3], expenseTBD[4]))
+                        data = [['ID', 'Amount', 'Description', 'Date Submitted']]
+                        amountString = ("%.2f" %expenseTBD[2])
+                        data.append([expenseTBD[0], amountString, expenseTBD[3], expenseTBD[4]])
+                        print(tabulate(data, tablefmt="grid"))
+
+                        #print("The expense seleced for deletion: ID-%i, Amount-%.2f, Description-%s, Date-%s" %(expenseTBD[0], expenseTBD[2], expenseTBD[3], expenseTBD[4]))
                         print("Are you sure you want to delete this expense?")
                         confirm = input("Enter YES to confirm: ")
                         if(confirm == "YES"):
@@ -354,11 +349,13 @@ class EmployeeApp:
                             print("Deletion aborted")
                             logging.info("Deletion aborted")
         except ValueError:
-            print("Invalid input for deleteExpense")
-            logging.error("Invalid input for deleteExpense")
+            print("Invalid input for deletion ID")
+            logging.error("Invalid input for deleteExpense\n")
         except sqlite3.Error as error:
-            print("SQL Error occured - ", error)
-            logging.error("SQL Error occured in deletion - ", error)
+            print("SQL Error occured: \n", error)
+            logging.error("SQL Error occured in deletion")
+        except Exception as e:
+            print(e)
         return
 
     #- As an employee, I want to view a history of all my approved and denied expenses 
@@ -388,7 +385,7 @@ class EmployeeApp:
                         #print("CURRENT STATUS FOR EXPENSE (ID-%i) IS: %s, reviewed by manager with ID-%i" %(row[0], approval[2], approval[3]))
                         #print("Comments made: %s\nOn date: %s\n" %(approval[4], approval[5]))
                         data.append(tablerow)
-                print(tabulate(data))
+                print(tabulate(data, tablefmt="grid"))
                 logging.info("Successfuly displayed approvals")
 
         except sqlite3.Error as error:
