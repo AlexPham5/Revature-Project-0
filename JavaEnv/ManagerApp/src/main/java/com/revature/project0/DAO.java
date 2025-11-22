a204d01840fcec47a19ff5f5aa63c0d66968e642
AlexPham5 <113805467+AlexPham5@users.noreply.github.com>
2025-11-21 22:56:00 -0600

    added logger to DAO

--- JavaEnv/ManagerApp/src/main/java/com/revature/project0/DAO.java
+++ JavaEnv/ManagerApp/src/main/java/com/revature/project0/DAO.java
@@ -1,4 +1,7 @@
 package com.revature.project0;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,13 +10,15 @@ import java.sql.Statement;
 //Will execute the SQL commands after ManagerApp verifies the inputs
 public class DAO {
     private String dbPath = "C:\\Users\\alex1\\Revature_work\\Project_0\\RevatureDatabase.db";
+    Logger logger = LoggerFactory.getLogger(ManagerApp.class);
 
     public DAO(){
-        //open db connection here? or just use with whenever opening connections?
+        //need logger?
     }
 
     //returns -1 if unsuccessful, otherwise returns the manager's id
     public int getCredentials(String username, String password){
+        logger.info("Attempting to find manager user credential's in db");
         
         return -1;
     }
